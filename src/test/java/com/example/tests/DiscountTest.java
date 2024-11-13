4bbfea11db551107030bc7a2bbaf8c1a8a9862f2
altuga <altuga@gmail.com>
2024-11-13 11:47:14 +0900

    lean the test process

--- src/test/java/com/example/tests/DiscountTest.java
+++ src/test/java/com/example/tests/DiscountTest.java
@@ -7,32 +7,5 @@ import org.junit.jupiter.api.Test;
 import com.example.Discount;
 
 public class DiscountTest {
-    @Test
-    void testWith101() {
-		Discount discount = new Discount();
-		
-		double total = discount.applyDiscount(101);
-
-        assertEquals(80.8, total, 0.01);
-    }
-
-//    @Test
-//    void testWith99() {
-//		Discount discount = new Discount();
-//		
-//		double total = discount.applyDiscount(99);
-//
-//        assertEquals(99.0, total, 0.01);
-//    }
-
-
-	@Test
-    void testWith100() {
-		Discount discount = new Discount();
-		
-		double total = discount.applyDiscount(100);
-
-        assertEquals(100, total, 0.01);
-    }
 
 }
