9f8ca54983a5d2f40777ef6a95b819e520c63f12
altuga <altuga@gmail.com>
2024-11-14 09:46:12 +0900

    testwith101

--- src/test/java/com/example/tests/DiscountTest.java
+++ src/test/java/com/example/tests/DiscountTest.java
@@ -8,4 +8,13 @@ import com.example.Discount;
 
 public class DiscountTest {
 
+
+    @Test
+    void testWith101() {
+    	Discount discount = new Discount();
+
+    	double total = discount.applyDiscount(101);
+
+        assertEquals(80.8, total, 0.01);
+    }
 }
